ac3f8804a78f0304efc5120caa63cc0a4bfeabd5
quotarbr <86331019+Bartolace@users.noreply.github.com>
2024-12-08 23:17:02 -0400

    Update buscar-cursos.php

--- Composer/buscador-cursos-alura/buscar-cursos.php
+++ Composer/buscador-cursos-alura/buscar-cursos.php
@@ -1,9 +1,12 @@
 <?php
 
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
  
 $client = new Client();
 
 $response = $client->request('GET', 'https://cursos.alura.com.br/category/programacao/php');
 
 $html =  $response->getBody();
+
+$crawler = new Crawler();
