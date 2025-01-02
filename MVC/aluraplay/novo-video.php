797e77ba575d313028175d981cfd233c3e2aa2c9
Gabriel Bartolace <gabrielll.freitas@hotmail.com>
2025-01-02 11:48:56 -0400

    insert and delete

---
+++ MVC/aluraplay/novo-video.php
@@ -0,0 +1,25 @@
+<?php
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo    = new PDO("sqlite:$dbPath");
+
+$url   = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
+$title = filter_input(INPUT_POST, 'titulo');
+
+if($url === false || $title === false){
+    header('Location: ./index.php?success=0');
+    exit();
+}
+
+try{
+    $sql  = 'INSERT INTO videos (url, title) VALUES (?, ?);'; 
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(1, $url);
+    $stmt->bindValue(2, $title);
+    
+    $stmt->execute();
+    header('Location: ./index.php?success=1');
+
+}catch(PDOException $e){
+    header('Location: ./index.php?success=0');
+}
